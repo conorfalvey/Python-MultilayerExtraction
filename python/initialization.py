b3bf68d0f13d1b5d275f59b7fb8b54038a430d9d
Conor Falvey <conorfalvey@Conors-MacBook-Pro.local>
2019-10-16 14:42:42 -0700

    Finish initialization.py

--- python/initialization.py
+++ python/initialization.py
@@ -1,19 +1,37 @@
+# initialization
+#
+# Function that identifies statistically significant vertex-layer communities in multilayer networks
+# @param graph:
+# @param prop_sample: The proportion of vertices to search over for initialization
+# @param m:
+# @param n:
+#
+# keywords: community detection, multilayer networks, configuration model, random graph models
+# @return: a list of vertex-set and layer combinations of statistically significance
+#
+# Basis: Wilson, James D., Palowitch, John, Bhamidi, Shankar, and Nobel, Andrew B. (2017) "Significance based
+#        extraction in multilayer networks with heterogeneous community structure." Journal of Machine Learning Research
+# Original R Code: James D. Wilson
+# Revised Python Code: Conor Falvey
 import networkx as nx
 import pandas as pd
 import math
 import random
 
-def initialization(adjacency, prop_sample, m, n):
+
+def initialization(graph, prop_sample, m, n):
+    # Cannot find analog to R's replicate() function, so a wrapper for random sample appends is used
     layer_set = list()
-    for i in range(1, math.ceil(prop_sample * n)):
-        layer_set[i] = random.sample(range(1, m), math.ceil(m/2))
-
-    graph = nx.parse_edgelist(pd.DataFrame(adjacency)[i].values.tolist()[, 1:2])
+    for i in range(0, math.ceil(prop_sample * n)):
+        layer_set.append(random.sample(range(0, m), math.ceil(m/2)))
+    # It doesn't appear that NetworkX has an analog for iGraph's neighborhood() function, so we iterate over the nodes
+    # and append the neighbors of each node to the node itself to model the output of iGraph's neighborhoods() function
     neighborhoods = list()
-    for n in graph.nodes_iter:
-        neighborhoods.append(nx.neighbors(graph, n))
-
+    for i in graph.nodes():
+        neighborhoods.append(list(i) + list(graph.neighbors(i)))
+    # Generate random sample of indices for out sample to keep
     keep_sample = random.sample(range(1, n), math.ceil(prop_sample * n))
-    neighborhoods = neighborhoods[keep_sample]
-
-    return list(vertex_set = neighborhoods, layer_set = layer_set)
+    # Subset neighborhoods with list comprehension for all values at keep_sample's indices
+    neighborhoods = [neighborhoods[i] for i in keep_sample]
+    # Return a DataFrame of the neighborhoods and the layer set to keep track of the vertex and layer sets
+    return pd.DataFrame({'vertex_set': neighborhoods, 'layer_set': layer_set})
