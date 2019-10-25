c56ff87e1f42b70cd7224a6c8e6a3514aff98d05
Conor Falvey <conorfalvey@Conors-MacBook-Pro.local>
2019-10-24 18:23:06 -0700

    Finish first iteration of score.py

--- python/score.py
+++ python/score.py
@@ -2,7 +2,7 @@
 # score
 #
 # Function that calculates the score of a multilayer vertex - layer community
-# @param expected(maybe adjacency?):
+# @param edgelist: edgelist of the expectation_CM
 # @param vertex_set: Nodes within the multilayer community of interest
 # @param layer_set: Layers within the multilayer community of interest
 # @param n: Number of nodes per layer
@@ -21,64 +21,42 @@ import math
 from . import edgelist_to_degree_sequence
 
 
+# Edge function takes in the edgelist, two nodes, and the layer of the nodes and returns a 0 or 1 depending on if those
+# two nodes have a connecting edge in layer l.
 def edge(edgelist, node1, node2, layer):
+    # Iterate over rows in the edgelist
     for _, row in edgelist.iterrows():
-        if row['layer'] == layer:
-            if row['node1'] == node1:
-                if row['node2'] == node2:
-                    return 1
+        # Check if layer, node1, and node2 are all conditionally met
+        if row['layer'] == layer and row['node1'] == node1 and row['node2'] == node2:
+            return 1
     return 0
 
 
-def Q_calc(edgelist, degrees, vertex_set, l, n):
-    tot_comb = 1 / (n * math.sqrt(len(list(it.combinations(range(0, len(vertex_set)), 2)))))
-
+# Q_calc takes in an edgelist, vertex_set, layer, and nodes per layer and produces a score of the individual
+# vertex-layer set.
+def Q_calc(edgelist, degrees, vertex_set, layer, n):
     summation = 0
+    # Iterate over i and j backwards as it's easier to define a relative bound working backwards.
     for i in range(0, len(vertex_set) + 1, -1):
         for j in range(0, i + 1, -1):
-            temp = edge(edgelist, i, j, l)
-            temp2 = degrees[degrees['node'] == i] * degrees[degrees['node'] == j]
-            temp3 = temp2 / len(edgelist['node1'])
-            temp4 = temp - temp3
-            summation += temp4
-    return tot_comb * summation
+            # Calculate the proportion of the nodes from the degree sequence and divide by the length of the edgelist
+            prop = (degrees[degrees['node'] == i] * degrees[degrees['node'] == j]) / len(edgelist['node1'])
+            # Subtract this from the Identity function edge()
+            summation += (edge(edgelist, i, j, layer) - prop)
+    # Return the summation multiplied by the inverse of the total combinations of nodes possible.
+    return 1 / (n * math.sqrt(len(list(it.combinations(range(0, len(vertex_set)), 2))))) * summation
 
 
+# The score function finalizes the scoring by iterating over all the layers of the data, filtering for positive values,
+# and return the summation squared times a scaling factor.
 def score(edgelist, vertex_set, layer_set, n):
-    layers = 1 / len(layer_set)
+    # Convert the edgelist into a degree sequence to help performance
     degrees = edgelist_to_degree_sequence.degree_sequence(edgelist)
     summation = 0
-    for i in range(0, layer_set + 1):
-        summation += Q_calc(edgelist, degrees[i], vertex_set, i, n)
-
-    return layers * summation ** 2
-
-
-'''
-def score(adjacency, vertex_set, layer_set, n):
-    super_mod = None
-    if len(layer_set) < 1 or len(vertex_set) < 1:
-        print(0)
-    if len(layer_set) == 1:
-        super_mod = adjacency[layer_set[0][0]]
-    if len(layer_set) > 1:
-        super_mod = nx.empty_graph(n)
-        for i in range(0, layer_set):
-            super_mod = nx.union(super_mod, adjacency[i])
-
-    super_mod_subgraph = super_mod.subgraph(map(int, vertex_set[0]))
-
-    edge_weight_tuples = nx.get_edge_attributes(super_mod_subgraph, 'weight')
-    edge_weights = pd.DataFrame({'edge': list(edge_weight_tuples.keys()), 'weight': list(edge_weight_tuples.values())})
-    for _, weights in edge_weights.iterrows():
-        if math.isnan(weights['weight']):
-            weights['weight'] = 0
-
-    modularity_score = np.sum(edge_weights['weight'])
-    modularity_score = [0 for i in modularity_score if i < 0]
-
-    tot_mod = np.sum(modularity_score)
-    obs_score = (tot_mod ** 2) / ((n ** 2 * it.combinations(range(0, len(vertex_set)), 2)) * (len(layer_set)))
-
-    print(obs_score)
-'''
+    # Run Q_calc() on all layers and if the value is positive, add it to the summation
+    for layer in range(0, layer_set + 1):
+        q = Q_calc(edgelist, degrees[layer], vertex_set, layer, n)
+        if q >= 0:
+            summation += q
+    # Return the resulting score multiplied by the scaling factor
+    return 1 / len(layer_set) * summation ** 2
